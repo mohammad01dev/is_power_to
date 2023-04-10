74e89666beb9e8041bded6250ad2fdd0fbe2d463
mohammad sadeghi <116999989+mohammadcoder82@users.noreply.github.com>
2023-04-10 08:29:13 +0330

    Add files via upload

---
+++ ex.py
@@ -0,0 +1,10 @@
+ex = int(input("?. "))
+
+def is_power_tow(num):
+    for i in range(0,num+1):
+        if i ** 2 == num and int(num):
+            print("Yess", i)
+            break
+        else:
+            print("No")
+is_power_tow(ex)
